5616de8705551075db919c6fdf176607bd61b76a
congenial-course-2234 <jakelozano94@gmail.com>
2020-05-13 23:01:17 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -3,20 +3,20 @@ def new_hash
 end
 
 def my_hash
-  hash1 = {
+  {
     one: "two"
   }
 end
 
 def pioneer
-  hash2 {
+  {
     name: "Grace Hopper"
   }
 end
 
 
 def id_hash_generator(number)
-  hash3 {
+{
     id: number
   }
 end
